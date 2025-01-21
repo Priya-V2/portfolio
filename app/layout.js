c532515bfd5c2f137bda7774810e242aaeb77396
Priya-V2 <priyav2.bct@gmail.com>
2025-01-21 18:45:07 +0530

    Update favicon and title, add project-related images to the public folder

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
+      <head>
+        <title>PRIYA V | Portfolio</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body>{children}</body>
     </html>
   );
