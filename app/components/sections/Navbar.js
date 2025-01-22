554942fb95635968556de62f60dc1c7a92642cce
Priya-V2 <priyav2.bct@gmail.com>
2025-01-22 23:03:48 +0530

    Add functionality to the menu icon in the navbar

--- app/components/sections/Navbar.js
+++ app/components/sections/Navbar.js
@@ -1,8 +1,16 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setDisplayMenu((prevState) => !prevState);
+  };
+
   return (
     <div>
       <nav className="flex justify-between items-center bg-primary px-4 lg:px-6 py-3 text-xs lg:text-sm">
@@ -19,7 +27,7 @@ const Navbar = () => {
           >
             Let's Talk
           </button>
-          <button type="button">
+          <button type="button" onClick={() => toggleMenu()}>
             <IoMenu className="w-6 h-6" />
           </button>
         </div>
@@ -47,6 +55,46 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
+      {displayMenu && (
+        <div
+          className={`flex flex-col items-center justify-center absolute top-0 left-0 bg-secondary w-screen h-screen text-base transform transition-transform duration-1000 ease-in-out ${
+            displayMenu ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <div className="absolute top-4 right-4">
+            <button
+              onClick={() => toggleMenu()}
+              className="text-dark-charcoal text-xl"
+            >
+              &times;
+            </button>
+          </div>
+          <div className="flex flex-col mx-10">
+            <ul className="flex flex-col text-center">
+              <Link href="#about" onClick={() => toggleMenu()}>
+                <li className="mb-4">About</li>
+              </Link>
+              <Link href="#skills" onClick={() => toggleMenu()}>
+                <li className="mb-4">Skills</li>
+              </Link>
+              <Link href="#projects" onClick={() => toggleMenu()}>
+                <li className="mb-4">Projects</li>
+              </Link>
+              <Link href="#education" onClick={() => toggleMenu()}>
+                <li>Education</li>
+              </Link>
+              <li>
+                <button
+                  type="button"
+                  className="font-extrabold tracking-wider text-white bg-highlight w-full mt-6 px-3 py-1.5 lg:px-4 lg:py-2 rounded-md uppercase"
+                >
+                  Let's Talk
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
